17d19aa7bf077be30820913421517035ccfd6d5b
HEYAHONG <2229388563@qq.com>
2022-10-01 21:37:33 +0800

    打印用户可用的Flash信息

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,13 @@
 #include <board.h>
 #include <RC.h>
 
+//内部flash变量
+extern "C" unsigned int USER_ADDR_START;
+extern "C" unsigned int USER_AREA_LEN;
+extern "C" unsigned int EX_USER_ADDR_START;
+extern "C" unsigned int EX_USER_AREA_LEN;
+
+
 int main(void)
 {
     /* set wifi work mode */
@@ -21,6 +28,10 @@ int main(void)
         rt_size_t total = 0, used = 0;
         rt_memory_info(&total, &used, NULL);
         printf("\r\nTotal Memory:%d Bytes,Used Memory:%d Bytes\r\n", (int)total, (int)used);
+    }
+
+    {
+        printf("User Flash Start=%08X length=%u\r\nEx User Flash Start=%08X Length=%u\r\n", USER_ADDR_START, USER_AREA_LEN, EX_USER_ADDR_START, EX_USER_AREA_LEN);
     }
 
     return RT_EOK;
